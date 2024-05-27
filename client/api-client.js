0174c26d5bb517f9ac701ce9291c6fc7e7b67db1
murasuke <42880820+murasuke@users.noreply.github.com>
2024-05-27 10:04:50 +0900

    Update api-client.js

--- client/api-client.js
+++ client/api-client.js
@@ -1,5 +1,7 @@
 import { createProxy } from './api-proxy.js';
+// dynamic importしたモジュールをProxy化
 const proxy = await createProxy('http://localhost:3000', './util.js');
+// proxyが"localhost:3000/util/strcat/?val1=aa&val2=bb"の呼び出しに置き換える
 const result = await proxy.strcat('aa', 'bb');
 console.log(result);
 
